fcbc1bb9c3ee110d165bd2252fec41ebba8f0fea
Peter <pebueku@gmail.com>
2021-08-18 23:02:39 +0100

    I don't even know

--- config/settings.py
+++ config/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['pebueku-heroku-resume.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['pebueku-heroku-resume.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
