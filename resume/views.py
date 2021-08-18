27c1fb98e76a8f01b272198cc8dd4f14856768d7
Peter <pebueku@gmail.com>
2021-08-18 22:10:56 +0100

    Initial commit

---
+++ resume/views.py
@@ -0,0 +1,44 @@
+from django.http import response
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+def home(request):
+    return render(request, 'resume\home.html')
+
+def send_gmail(request):
+    if request.method=='POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        form_subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        # print(name, subject, email, message)
+
+        subject = f'[Django Resume]-{form_subject}'
+        message += f'\n\nName:\t{name.title()}\nEmail:\t{email}\nPhone:\t{phone}'
+        send_mail(
+            subject,
+            message,
+            'pebueku.test@gmail.com',
+            ['pebueku@gmail.com']
+            
+        )
+
+
+        reply_subject = 'Thanks for reaching out!'
+        reply_message = f'Goodday {name.title()},\nThank you for reaching out, I\'ll be sure to get back to you soon.'
+
+        send_mail(
+            reply_subject,
+            reply_message,
+            'pebueku.test@gmail.com',
+            [email]
+
+        )
+
+
+        return HttpResponseRedirect(reverse('home'))
+    else:
+        return HttpResponse('Invalid request')
